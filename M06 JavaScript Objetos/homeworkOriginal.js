4995a913dbb2e425dde07eb7a048befd7c234fbc
Nico Garcia <nicolasgarcia9812@hotmail.com>
2023-08-07 19:13:36 -0300

    Commit de prueba

--- M06 JavaScript Objetos/homeworkOriginal.js
+++ M06 JavaScript Objetos/homeworkOriginal.js
@@ -7,6 +7,14 @@ function crearGato(nombre, edad) {
   // La propiedad "meow" será una función que retorne el string: "Meow!".
   // Retornar el objeto.
   // Tu código:
+  var gato = {
+    nombre,
+    edad,
+    meow: function () {
+      return "Meow!";
+    },
+  };
+  return gato;
 }
 
 function nuevoUsuario(nombre, email, password) {
@@ -14,6 +22,8 @@ function nuevoUsuario(nombre, email, password) {
   // Este debe tener las propiedades: "nombre", "email" y "password" con sus respectivos valores.
   // Retornar el objeto.
   // Tu código:
+  var usuario = { nombre, email, password };
+  return usuario;
 }
 
 function agregarPropiedad(objeto, propiedad) {
@@ -22,6 +32,8 @@ function agregarPropiedad(objeto, propiedad) {
   // Esta propiedad será igual al valor `null`.
   // Retornar el objeto.
   // Tu código:
+  objeto[propiedad] = null;
+  return objeto;
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -29,36 +41,43 @@ function invocarMetodo(objeto, metodo) {
   // Esta propiedad contiene una función en su interior. Debes invocarla/ejecutarla.
   // [NOTA]: no necesitar retornar nada.
   // Tu código:
+  objeto[metodo]();
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // El parámetro "objetoMisterioso" posee una propiedad con el nombre "numeroMisterioso".
   // Debes multiplicar este número por 5 y retornar el resultado.
   // Tu código:
+  return objetoMisterioso.numeroMisterioso * 5;
 }
 
 function eliminarPropiedad(objeto, propiedad) {
   // El parámetro "propiedad" es una propiedad del objeto que recibes.
   // Debes eliminarla del objeto y retornarlo finalmente.
   // Tu código:
+  delete objeto[propiedad];
+  return objeto;
 }
 
 function tieneEmail(objetoUsuario) {
   // Verifica si el "objetoUsuario", en su propiedad "email", posee un valor definido.
   // En ese caso retornar true. Caso contrario, false.
   // Tu código:
+  return objetoUsuario.hasOwnProperty("email") && objetoUsuario.email !== null;
 }
 
 function tienePropiedad(objeto, propiedad) {
   // Verifica si el objeto recibido posee una propiedad con el mismo nombre que el parámetro "propiedad".
   // En ese caso retornar true. Caso contrario, false.
   // Tu código:
+  return objeto.hasOwnProperty(propiedad);
 }
 
 function verificarPassword(objetoUsuario, password) {
   // Verifica si la propiedad "password" del "objetoUsuario" coincide con el parámetro "password".
   // En ese caso retornar true. Caso contrario, false.
   // Tu código:
+  return objetoUsuario.password === password;
 }
 
 function actualizarPassword(objetoUsuario, nuevaPassword) {
@@ -85,7 +104,6 @@ function pasarUsuarioAPremium(objetoMuchosUsuarios) {
   // Define esta propiedad de todos los usuarios como true.
   // Retornar el arreglo.
   // Tu código:
-  // ARREGLO -> OBJETO -> PROPIEDAD esPremium = true
   objetoMuchosUsuarios.forEach((usuario) => {
     usuario.esPremium = true;
   });
@@ -98,10 +116,9 @@ function sumarLikesDeUsuario(objetoUsuario) {
   // Cada post posee una propiedad llamada "likes". Esta propiedad es un número.
   // Debes sumar los likes de todos los post y retornar el resultado.
   // Tu código:
-  // objetoUsuario -> posts -> post -> likes (numero)
   var suma = 0;
-  objetoUsuario.posts.forEach((post) => {
-    suma = suma + post["likes"]; //
+  objetoUsuario["posts"].forEach((post) => {
+    suma = suma + post["likes"];
   });
   return suma;
 }
@@ -117,9 +134,10 @@ function agregarMetodoCalculoDescuento(objetoProducto) {
   // PorcentajeDeDescuento ---> 0.2
   // Precio final ---> 8
   // Tu código:
+
   objetoProducto.calcularPrecioDescuento = function () {
-    var descuento = this.precio * this.porcentajeDeDescuento;
-    var precioFinal = this.precio - descuento;
+    const descuento = this.precio * this.porcentajeDeDescuento;
+    const precioFinal = this.precio - descuento;
     return precioFinal;
   };
   return objetoProducto;
